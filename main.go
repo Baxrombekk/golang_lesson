07b760eae8711dbb676d924b9efa46d1693c441c
Otaqulov Bahrom <baxrombekotaqulov2024@gmail.com>
2025-11-19 16:53:09 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,298 @@
+//package main
+//
+//import (
+//	"fmt"
+//	"math"
+//)
+//
+//func main() {
+//
+//	s := "gopher"
+//	fmt.Printf("Hello and welcome, %s!\n", s)
+//
+//	for i := 1; i <= 5; i++ {
+//		fmt.Println("i =", 100/i)
+//		fmt.Println("one", 1)
+//		fmt.Println("two", 2)
+//		fmt.Println("three point five", 3.5)
+//
+//		integerValue := 10
+//		flooatValue := float64(integerValue)
+//		fmt.Printf("The value of integer %d value is: %f\n", flooatValue, integerValue)
+//	}
+//	fmt.Println("question")
+//	pi := 3.1415926
+//	roundedPi := math.Round(pi)
+//	fmt.Printf("Rounded Pi: %.0f\n", roundedPi)
+//}
+
+package main
+
+import "fmt"
+
+//	func main() {
+//		fmt.Println("True", true)
+//		fmt.Println("False", false)
+//
+//		fmt.Println("Exercise")
+//
+//		var a bool
+//		fmt.Printf("Is the user logged in ? %t\n", a)
+//
+//		fmt.Println("lemoncode")
+//		fmt.Println("youtube")
+//		fmt.Println(len("kiaxb"))
+//		fmt.Println("salom"[2])
+//
+//		firstname := string("Baxrom")
+//		lastname := string("Otaqulov")
+//		fmt.Println("fullname = " + firstname + " " + lastname)
+//
+// }
+func main() {
+	//fmt.Println("veriables")
+	//
+	//name := "my name"
+	//fmt.Println(name)
+	//name = "last name"
+	//fmt.Println(name)
+	//
+	//var name2 = "my pc"
+	//fmt.Println(name2)
+	//
+	//name2 = "my old pc"
+	//fmt.Println(name2)
+
+	//const firstName = "Mary Jane"
+	//const lastName = "Smith"
+
+	//fmt.Println(day, week)
+	//
+	//var value32 int32 = 32768
+	//var value64 int64 = int64(value32)
+	//var value16 int16 = int16(value64)
+	//
+	//fmt.Println(value32)
+	//fmt.Println(value64)
+	//fmt.Println(value16)
+
+	//yusuf := ("limoncode")
+	//e := yusuf[0]
+	//b := string(e)
+	//fmt.Println(b)
+	//intVar := 48
+	//floatVar := float64(intVar) + 5.8
+	//fmt.Println(floatVar, intVar)
+
+	//var a = 10
+	//var b = 10
+	//var c = a + b
+	//fmt.Println(c)
+
+	//var x, y = 15, 20
+	//x = y
+	//fmt.Println(x)
+	//
+	//x = 15
+	//x += y
+	//fmt.Println(x)
+
+	//a, b := 10, 5
+	//fmt.Printf("qoshish %d\n", a+b)
+	//fmt.Printf("ayirish%d\n", a-b)
+
+	//total := 50
+	//total += 10
+	//fmt.Println(total)
+	//
+	//total -= 10
+	//fmt.Println(total)
+	//
+	//total *= 10
+	//fmt.Println(total)
+	//
+	//total /= 10
+	//fmt.Println(total)
+
+	//fmt.Println("arraylar")
+	//
+	//x := [5]int{1, 2, 3, 4, 5}
+	//fmt.Println(x)
+	//y := [...]int{1, 2, 3, 4, 5, 6}
+	//fmt.Println(y[2])
+	//
+	//fmt.Println("Array 2")
+	//array2 := [5]string{"a", "b", "c", "d", "e"}
+	//fmt.Println(array2[2])
+
+	//arr := []string{"usa", "uk", "USA"}
+	//fmt.Println(arr)
+	//fmt.Printf("strarr \tlen %v \tcap %v\n", len(arr), cap(arr))
+	//var stringslice = make([]string, 10, 20)
+	////fmt.Printf("strarr \tlen %v \tcap %v\n", len(stringslice), cap(stringslice))
+	//
+	//arr := []int{1, 12, 13, 114}
+	//fmt.Println(arr[0])
+	//fmt.Println(arr[1])
+	//fmt.Println(arr[0:3])
+	//stringslice = append(stringslice, "Hello")
+	//fmt.Println(stringslice)
+	//stringslice3 := make([]string, 10, 15)
+	//copy(stringslice3, stringslice)
+	//fmt.Println(stringslice3)
+
+	//var arr = make(map[string]int)
+	//arr["sancho"] = 1
+	//arr["muzaffar"] = 2
+	//fmt.Println(arr)
+	//fmt.Println(arr["muzaffar"])
+	//delete(arr, "muzaffar")
+
+	//var age = map[string]int{
+	//	"Ali":  20,
+	//	"Vali": 25,
+	//	"Soli": 30,
+	//}
+	//fmt.Println(age["Ali"])
+
+	//name := "john"
+	//if length := len(name); length > 5 {
+	//	fmt.Println("your name is very long")
+	//} else {
+	//	fmt.Println("your name is good")
+	//}
+
+	//score := 75
+	//
+	//if score >= 60 {
+	//	fmt.Println("good")
+	//} else {
+	//	fmt.Println("Fail")
+	//}
+
+	//bal := 2
+	//
+	//switch bal {
+	//case 1:
+	//	fmt.Println("Bir")
+	//case 2:
+	//	fmt.Println("ikki")
+	//default:
+	//	fmt.Println("<UNK>")
+	//}
+
+	//	count := 1
+	//	for count <= 10 {
+	//		fmt.Println(count)
+	//		count++
+	//	}
+
+	//name := []string{"Alice", "Bob", "Alice"}
+	//
+	//for index, elemnt := range name {
+	//	fmt.Println(elemnt, index, elemnt)
+	//}
+
+	//fruicts := []string{"Banana", "apple", "cherry"}
+	//
+	//for index, fruic := range fruicts {
+	//	fmt.Println(fruic, index+1)
+	//}
+	//	sayHello()
+	//	add(1, 6)
+	//	resault := sum(5, 2)
+	//	fmt.Println(resault)
+	//
+	//	var a, p int
+	//	a, p = rectangle(10, 30)
+	//	fmt.Println("Area", a)
+	//	fmt.Println("Prettier", p)
+	//
+	//	name, lastname := getFullname()
+	//	fmt.Println(name, lastname)
+	//	sayHelloWithFilter("john", spamFilter)
+	//	sayHelloWithFilter("dog", spamFilter)
+	//	kubi, kvadrati := daraja3(3)
+	//
+	//	fmt.Printf("kubi: %v, kvadrati: %v\n", kubi, kvadrati)
+	//	sort(19, 18, 122)
+	//	fukt := fukturial(5)
+	//	fmt.Println("Fuktional", fukt)
+	//	massiv := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 133, 135}
+	//	s := 0
+	//	for index, i := range massiv {
+	//		if i%2 == 1 {
+	//			fmt.Println("Massiv", i, index)
+	//			s += 1
+	//			if s == 7 {
+	//				fmt.Println("Massiv 7 ta boldi")
+	//				break
+	//			}
+	//		}
+	//	}
+	//}
+	massiv := []int{2, 1, 1, 15, 4, 8, 7, 6, 5}
+	for i := 0; i < len(massiv)-1; i++ {
+		for j := 0; j < len(massiv)-1-i; j++ {
+			if massiv[j] > massiv[j+1] {
+				massiv[j], massiv[j+1] = massiv[j+1], massiv[j]
+			}
+		}
+	}
+
+	fmt.Println(massiv)
+
+	//func sayHello() {
+	//	fmt.Println("Hello Func")
+	//}
+	//func add(a int, b int) {
+	//	total := a + b
+	//	fmt.Println(total)
+	//}
+	//func sum(a int, b int) int {
+	//	return a + b
+	//}
+	//
+	//func rectangle(a int, b int) (area int, parametr int) {
+	//	parametr = 2 * (a + b)
+	//	area = a * b
+	//	return
+	//}
+	//func getFullname() (name string, lastname string) {
+	//	name = "david"
+	//	lastname = "jony"
+	//	return name, lastname
+	//}
+	//func spamFilter(name string) string {
+	//	if name == "dog" {
+	//		return "..."
+	//	} else {
+	//		return name
+	//	}
+	//}
+	//func sayHelloWithFilter(name string, filter func(string) string) {
+	//	fmt.Println("Hello", filter(name))
+	//}
+	//func daraja3(a int) (int, int) {
+	//	return a * a * a, a * a
+	//}
+	//func sort(a int, b int, c int) {
+	//	if a > b {
+	//		a, b = b, a
+	//	}
+	//	if b > c {
+	//		b, c = c, b
+	//	}
+	//	if a > b {
+	//		a, b = b, a
+	//	}
+	//
+	//	fmt.Println(a, b, c)
+	//}
+	//func fukturial(a int) int {
+	//	s := 1
+	//	for i := 1; i < a+1; i++ {
+	//		s *= i
+	//	}
+	//	return s
+}
